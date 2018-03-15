d29f19e56a2a8fb2ba97b36c32e4637612e73ed9
Krzysztof Owsiany <gocom@o2.pl>
2018-03-15 21:46:33 +0100

    Base for events in wpf

---
+++ WpfRXExamples/Observables/Unsubscribe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WpfRXExamples.Observables
+{
+	internal class Unsubscribe<T> : IDisposable
+	{
+		private readonly IObserver<T> _observer;
+		private readonly IList<IObserver<T>> _subscribents;
+
+		public Unsubscribe(IObserver<T> observer, IList<IObserver<T>> subscribents)
+		{
+			_observer = observer;
+			_subscribents = subscribents;
+		}
+
+		public void Dispose()
+		{
+			_subscribents.Remove(_observer);
+		}
+	}
+}
