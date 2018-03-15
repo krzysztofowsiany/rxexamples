4e61dd1c57df11cfd0d44605adf3adb8a0191143
Krzysztof Owsiany <gocom@o2.pl>
2018-03-15 22:06:58 +0100

    AutoFac example

--- RXExamples/Application.cs
+++ RXExamples/Application.cs
@@ -1,29 +1,42 @@
 ﻿using System;
+using System.Reactive.Linq;
 using System.Threading;
 
 namespace RXExamples
 {
 	public class Application
 	{
-		public Application()
-		{
+		private object _obj;
 
+		public Application(Object obj, Clock clock, Clock2 clock2)
+		{
+			_obj = obj;
+			Console.CursorVisible = false;
 		}
 
 		public void WaitForKeyPress()
 		{
+			Console.SetCursorPosition(0, Console.WindowHeight - 1);
 			Console.WriteLine("Press space key to exit");
+
+			var counter = 0;
 
 			ConsoleKeyInfo consoleKeyInfo;
 			do
 			{
 				while (!Console.KeyAvailable)
 				{
+					lock (_obj)
+					{
+						Console.SetCursorPosition(0, Console.WindowHeight - 2);
+						Console.WriteLine(counter++);
+					}
+
 					Thread.Sleep(150);
 				}
 				consoleKeyInfo = Console.ReadKey(true);
 			}
-			while (consoleKeyInfo.Key != ConsoleKey.Spacebar);
+			while (consoleKeyInfo.Key != ConsoleKey.Spacebar) ;
 		}
 	}
 }
---
+++ RXExamples/Interval1s.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace RXExamples
+{
+	public class Interval1s
+	{
+		public IObservable<long> Timer { get; private set; }
+
+		public Interval1s()
+		{
+			Timer = Observable.Interval(TimeSpan.FromSeconds(1));
+		}
+	}
+}
