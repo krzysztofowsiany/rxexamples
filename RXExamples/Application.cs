a652bd907c73919c2a6434e83e060620ebd24d16
Krzysztof Owsiany <gocom@o2.pl>
2018-03-15 21:55:33 +0100

    Timers example

--- RXExamples/Application.cs
+++ RXExamples/Application.cs
@@ -1,17 +1,39 @@
 ﻿using System;
+using System.Reactive.Linq;
 
 namespace RXExamples
 {
 	public class Application
 	{
+		private IDisposable _subscribent1;
+		private IDisposable _subscribent2;
+
 		public Application()
 		{
+			var interval = Observable.Interval(TimeSpan.FromSeconds(1));
+
+			_subscribent1 = interval
+				.TimeInterval()
+				.Subscribe(tick =>
+				{
+					Console.WriteLine($"Tick 1: {tick}");
+				});
+
+			_subscribent2 = interval
+				.Sample(TimeSpan.FromSeconds(4))
+				.Subscribe(tick =>
+				{
+					Console.WriteLine($"Tick 2: {tick}");
+				});
 		}
 
 		public void WaitForKeyPress()
 		{
 			Console.WriteLine("Press any key to exit");
 			Console.ReadKey();
+
+			_subscribent1.Dispose();
+			_subscribent2.Dispose();
 		}
 	}
 }
