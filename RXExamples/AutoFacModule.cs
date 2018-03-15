de0f04d19278fb275da53f34c3361a1c10c26932
Krzysztof Owsiany <gocom@o2.pl>
2018-03-15 22:06:58 +0100

    AutoFac example

--- RXExamples/AutoFacModule.cs
+++ RXExamples/AutoFacModule.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+using System.Reactive.Linq;
 using Autofac;
 
 namespace RXExamples
@@ -8,6 +10,19 @@ namespace RXExamples
 		protected override void Load(ContainerBuilder builder)
 		{
 			builder.RegisterType<Application>();
+
+			builder.RegisterInstance(new Object())
+				.SingleInstance();
+
+			builder.RegisterType<Clock>();
+			builder.RegisterType<Clock2>();
+
+			builder.RegisterInstance(Observable.Interval(TimeSpan.FromMilliseconds(10)))
+				.As<IObservable<long>>()
+				.SingleInstance();
+
+			builder.RegisterType<Interval1s>()
+				.SingleInstance();
 		}
 	}
 }
