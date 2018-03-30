12345b021607e5fe3ddc5b247a4bc2cd0c6c9ae1
Krzysztof Owsiany <gocom@o2.pl>
2018-03-30 23:44:01 +0200

    Fix colors

--- RXExamples/Program.cs
+++ RXExamples/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace RXExamples
+﻿using System;
+
+namespace RXExamples
 {
 	class Program
 	{
 		static void Main()
 		{
+			Console.BackgroundColor = ConsoleColor.Black;
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.Clear();
+
 			var application = new Application();
 			application.WaitForKeyPress();
 		}
